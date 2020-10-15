df6a1385a58c71c6b260336a30a3fae2fa5d48f3
Meylis Ovezov <meylis.ovezov.98@gmail.com>
2020-10-15 08:07:21 -0700

    some changes

--- js/app.js
+++ js/app.js
@@ -1,17 +1,89 @@
-$('.owl-carousel').owlCarousel({
-    rtl:true,
-    loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:1
-        },
-        1000:{
-            items:1
+
+var textItems = document.getElementsByClassName("text-item");
+var photoItems = document.getElementsByClassName("photo-item");
+
+var x=0;
+var y=0
+
+document.getElementById("left-arrow").style.display = "none";
+
+function rightControl(){
+    textRight();
+    photoRight();
+}
+
+function leftControl(){
+    textLeft();
+    photoLeft();
+}
+
+function textRight(){
+    if (x>0){
+        document.getElementById("home-btn").style.display = "none";
+        document.getElementById("left-arrow").style.display = "initial";
+
+        textItems[x-1].classList.add("hidden");
+        textItems[x].classList.remove("hidden");
+        x++;
+        if (textItems.length==x){
+            document.getElementById("right-arrow").style.display = "none";
+            document.getElementById("home-btn-right").style.display = "initial";
+        }
+    }else{
+        document.getElementById("home").style.display = "none";
+        document.getElementById("main").style.display = "flex";
+        textItems[x].classList.remove("hidden");
+        document.getElementById("left-arrow").style.display = "none";
+        x++;
+        if(x==1){
+        document.getElementById("home-btn").style.display = "initial";
         }
     }
-})
+}
+
+function textLeft(){
+    if (x>0){
+        document.getElementById("home-btn-right").style.display = "none";
+        document.getElementById("right-arrow").style.display = "initial";
+        x--;
+        textItems[x].classList.add("hidden");
+        textItems[x-1].classList.remove("hidden");
+        if (x==1){
+            document.getElementById("left-arrow").style.display = "none";
+            document.getElementById("home-btn").style.display = "initial";
+        }
+    }  
+}
+
+function photoRight(){
+    if (y>0){
+        photoItems[y-1].classList.add("hidden");
+        photoItems[y].classList.remove("hidden");
+        y++;
+    }else{
+        photoItems[y].classList.remove("hidden");
+        return y++;
+    }
+}
+
+function photoLeft(){
+    if (y>0){
+        y--;
+        photoItems[y].classList.add("hidden");
+        photoItems[y-1].classList.remove("hidden");
+    }
+    
+    // photoItems[y].classList.add("hidden");
+}
+
+// function takeToHomeStart(){
+//     document.getElementById("home").style.display = "initial";
+//     document.getElementById("main").style.display = "none";
+//     return x=0, y=0;
+// }
+
+// function takeToHomeEnd(){
+//     document.getElementById("home").style.display = "initial";
+//     document.getElementById("main").style.display = "none";
+//     return x=0, y=0;
+// }
